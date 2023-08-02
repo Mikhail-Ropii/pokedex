53d592703469218ee5ea588caec29dfcf73a1e72
Mikhail-Ropii <86194177+Mikhail-Ropii@users.noreply.github.com>
2023-08-02 14:47:42 +0300

    Delete src directory

--- src/components/container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import css from "./styles.module.scss";
-
-interface ContainerProps {
-  children: React.ReactNode;
-}
-
-export const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className={css.container}>{children}</div>;
-};
--- src/utils/capitalizeFirstLetter.ts
+++
@@ -1,4 +0,0 @@
-export const capitalizeFirstLetter = (inputString: string): string => {
-  const [firstLetter, ...rest] = inputString;
-  return firstLetter.toUpperCase() + rest.join("");
-};
